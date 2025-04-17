2537be26fc5bafaeb320e6aa37dec8a7a5650911
Manny <novoamanuel@ymail.com>
2025-04-17 12:13:29 -0500

    Added cors to open up.

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 app.use(cors({
-  origin: '*',
+  origin: 'http://192.168.4.58:3000',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }))
@@ -17,13 +17,6 @@ app.use(cors({
 // backend routes
 app.use('/api', require('./routes/api/'));
 app.use('/api/sync', require('./routes/api/sync'));
-// app.use('/api/dc/customers', require('./routes/api/DreamCatchers/customers'));
-// app.use('/api/dc/loyalty', require('./routes/api/DreamCatchers/loyalty'));
-// app.use('/api/dc/hubspot', require('./routes/api/DreamCatchers/hubspot'));
-
-// app.listen(PORT, () => {
-//   console.log(`Server listening on port ${PORT}`);
-// });
 
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on http://0.0.0.0:${PORT}`);
